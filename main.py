a77a71bd9c5e2c77ed981e01dc17bab88771013f
Vivian Pao <vivianpao16@gmail.com>
2020-09-02 22:34:19 +1000

    Final clean up commit

--- main.py
+++ main.py
@@ -9,32 +9,33 @@ from Sociogram import *
 # USER INPUTS
 # ***************************************************************
 TOPIC = "western sydney airport"
-TWEET_LIM = 100				# The number of tweets to scrape from Twitter before stopping.
-TOP_N = 10					# How many major users to identify. If TOP_N = 10, code names top 10 users.
+TWEET_LIM = 100
+TOP_N = 10
 
 REUSE_DATA = False
 
 # ***************************************************************
 # TWITTER SCRAPING
 # ***************************************************************
-
 if REUSE_DATA == False:
 	collectedData = scrapeTopic(TOPIC,TWEET_LIM)
 	collectedData.to_csv('data.csv',index=False)
-else:
+else:	# Dicts by default in string format. Rewrite the column with real dicts.
 	collectedData = pd.read_csv('data.csv')
-	weightedLinks = collectedData['reply_to'].tolist()	# Link of dictionaries
+	weightedLinks = collectedData['reply_to'].tolist()	# List of dicts
 	columnIndex = collectedData.columns.get_loc('reply_to')
-	for i in range(len(weightedLinks)):		# Reading in the dictionaries from string format
-		collectedData.iloc[i,columnIndex] = [ast.literal_eval(weightedLinks[i])]	# [i]['reply_to'] is a COPY. need to reference properly!
+	for i in range(len(weightedLinks)):		
+		collectedData.iloc[i,columnIndex] = [ast.literal_eval(weightedLinks[i])]
 
 # ***************************************************************
 # VISUALISATION
 # ***************************************************************
-
 mySociogram = Sociogram(collectedData)
 mySociogram.drawNetwork(TOP_N)
 mySociogram.saveSummary('summary.csv') # (TOPIC +' summary.csv')
+
+
+
 
 # TO DO:
 # - Remove overlapping, get better layout
