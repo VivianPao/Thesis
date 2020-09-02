c1c6d7e0f7fb02a042f124e1fb8bd10214aeea23
Vivian Pao <vivianpao16@gmail.com>
2020-09-03 01:00:44 +1000

    Altered names of output files and other refactoring. Constants and user inputs swapped around in order.

--- main.py
+++ main.py
@@ -8,22 +8,23 @@ from twitterScraping import *
 from Sociogram import *
 
 # ***************************************************************
+# CONSTANTS
+# ***************************************************************
+[IN_CEN,OUT_CEN,EIG_CEN,CLOSE_CEN,BTWN_CEN] = range(0,5)
+CENTRALITY = {'in':IN_CEN,'out':OUT_CEN,'eigenvector':EIG_CEN,'closeness':CLOSE_CEN,'betweenness':BTWN_CEN}
+
+# ***************************************************************
 # USER INPUTS
 # ***************************************************************
 reuseData = False
-topic = "western sydney airport"
 tweetLim = 100	# Max number of tweets to scrape
+
+topic = "western sydney airport"
 topN = 10		# Number of the top influential users to label on visualisation
 centralityType = 'eigenvector'	# Choose from 'in','out','eigenvector','closeness','betweeness'
 dates = None
 
 # dates = ['2020-01-01','2020-02-01'] # Dates in form ['YYYY-MM-DD','YYYY-MM-DD']
-
-# ***************************************************************
-# CONSTANTS
-# ***************************************************************
-[IN_CEN,OUT_CEN,EIG_CEN,CLOSE_CEN,BTWN_CEN] = range(0,5)
-CENTRALITY = {'in':IN_CEN,'out':OUT_CEN,'eigenvector':EIG_CEN,'closeness':CLOSE_CEN,'betweenness':BTWN_CEN}
 
 # ***************************************************************
 # TWITTER SCRAPING
@@ -48,10 +49,10 @@ mySociogram = Sociogram(collectedData)
 # Create figure and save as image
 f = plt.figure()
 plt.axis('off')
-plt.title(topic.title(),fontweight="bold")
+plt.title(topic.title(),fontweight='bold')
 mySociogram.calcFeatures(topN,centralitySelected)
 mySociogram.drawNetwork()
-f.savefig(topic.title() + " sociogram major.jpg")
+f.savefig(topic.title() + ' ' + centralityType + ' sociogram.jpg')
 
-mySociogram.saveSummary(topic.title() + 'summary.csv') # (topic +' summary.csv')
+mySociogram.saveSummary(topic.title() + ' ' + centralityType + ' summary.csv') # (topic +' summary.csv')
 
